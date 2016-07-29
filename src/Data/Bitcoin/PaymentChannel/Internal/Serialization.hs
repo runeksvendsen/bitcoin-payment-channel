2a97f28d3218731477c7ac7e83691270b7567130
Rune K. Svendsen <runesvend@gmail.com>
2016-07-29 20:29:52 +0200

    add BitcoinLockTime To/FromJSON instance; clean Serialization up a bit

--- src/Data/Bitcoin/PaymentChannel/Internal/Serialization.hs
+++ src/Data/Bitcoin/PaymentChannel/Internal/Serialization.hs
@@ -2,58 +2,38 @@
 
 module Data.Bitcoin.PaymentChannel.Internal.Serialization where
 
-import Data.Bitcoin.PaymentChannel.Internal.Types
-import Data.Bitcoin.PaymentChannel.Internal.Util
-    (BitcoinAmount(..), toWord64, deserEither, toHexString)
+import           Data.Bitcoin.PaymentChannel.Internal.Types
+import           Data.Bitcoin.PaymentChannel.Internal.Util
+                            (BitcoinAmount(..), toWord64, deserEither, toHexString,
+                             parseBitcoinLocktime, BitcoinLockTime(..), toWord32)
 
-import            Data.Aeson (Value(Number), FromJSON(..), ToJSON(..),
+import           Data.Aeson (Value(Number), FromJSON(..), ToJSON(..),
                               withText, withScientific)
-import            Data.Aeson.Types (Parser)
-import            Data.Scientific (Scientific, scientific, toBoundedInteger)
-import            Data.Text.Encoding       (decodeLatin1, encodeUtf8)
-import            Data.ByteString.Lazy (toStrict, fromStrict)
-import qualified  Data.Binary as Bin
-import qualified  Data.Binary.Put as BinPut
-import qualified  Data.Binary.Get as BinGet
-import qualified  Data.ByteString.Base64.URL as B64
+import           Data.Aeson.Types (Parser)
+import           Data.Scientific (Scientific, scientific, toBoundedInteger)
+import           Data.Text.Encoding       (decodeLatin1, encodeUtf8)
+import           Data.ByteString.Lazy (toStrict, fromStrict)
+import qualified Data.Binary as Bin
+import qualified Data.Binary.Put as BinPut
+import qualified Data.Binary.Get as BinGet
+import qualified Data.ByteString.Base64.URL as B64
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
+import           Data.Word (Word64)
 import           Data.EitherR (fmapL)
 import           Data.Typeable
 
-instance Show Payment where
-    show (CPayment val sig) =
-        "<Payment: valLeft=" ++ show val ++
-        ", sig=" ++ toHexString (toStrict $ Bin.encode sig) ++ ">"
-
--------JSON--------
 
 
-padToMod4 :: B.ByteString -> B.ByteString
-padToMod4 bs =
-    let
-        lastGroupSize = B.length bs `mod` 4
-        numPadChars = if lastGroupSize > 0 then 4 - lastGroupSize else 0
-    in
-        B.concat [bs, B.replicate numPadChars 61] -- 61: '=' ASCII
+---- JSON
+instance ToJSON BitcoinLockTime where
+    toJSON blt = Number $ scientific
+        (fromIntegral $ toWord32 blt) 0
 
-b64Encode :: Bin.Binary a => a -> B.ByteString
-b64Encode = B64.encode . toStrict . Bin.encode
-
-b64Decode :: (Typeable a, Bin.Binary a) => B.ByteString -> Either String a
-b64Decode b64 =
-    concatErr "failed to deserialize parsed base64 data: " . deserEither . BL.fromStrict =<<
-    (concatErr "failed to parse base64 data: ") (b64Decode b64)
-        where
-            b64Decode = B64.decode . padToMod4
-            concatErr e = fmapL (e ++)
-
-txtB64Encode :: Bin.Binary a => a -> T.Text
-txtB64Encode = decodeLatin1 . b64Encode
-
-txtB64Decode :: (Typeable a, Bin.Binary a) => T.Text -> Parser a
-txtB64Decode = either fail return . b64Decode . encodeUtf8
+instance FromJSON BitcoinLockTime where
+    parseJSON = withScientific "BitcoinLockTime" $
+        fmap (parseBitcoinLocktime . fromIntegral) . parseJSONInt
 
 instance ToJSON Payment where
     toJSON = toJSON . txtB64Encode
@@ -75,19 +55,8 @@ instance ToJSON PaymentChannelState where
 instance FromJSON PaymentChannelState where
     parseJSON = withText "PaychanState" txtB64Decode
 
--- Needed to convert from Scientific
-instance Bounded BitcoinAmount where
-    minBound = CMoneyAmount 0
-    maxBound = CMoneyAmount $ round $ 21e6 * 1e8
 
-parseJSONInt :: Scientific -> Parser Integer
-parseJSONInt s =
-    case toBoundedInteger s of
-        Just (CMoneyAmount i) -> return i
-        Nothing -> fail $ "failed to decode JSON number to integer. data: " ++ show s
-
-
-------BINARY--------
+--- Binary
 instance Bin.Binary PaymentChannelState where
     put (CPaymentChannelState par fti payConf valLeft (Just sig)) =
         Bin.put par >> Bin.put fti >> Bin.put payConf >> Bin.put valLeft
@@ -125,3 +94,53 @@ instance Bin.Binary PaymentSignature where
         >> Bin.put (psSigHash ps)
     get = CPaymentSignature <$> Bin.get <*> Bin.get
 
+
+--- Misc.
+instance Show Payment where
+    show (CPayment val sig) =
+        "<Payment: valLeft=" ++ show val ++
+        ", sig=" ++ toHexString (toStrict $ Bin.encode sig) ++ ">"
+
+    -- Needed to convert from Scientific
+instance Bounded BitcoinAmount where
+    minBound = CMoneyAmount 0
+    maxBound = CMoneyAmount $ round $ 21e6 * 1e8
+
+
+--- Util
+b64Encode :: Bin.Binary a => a -> B.ByteString
+b64Encode = B64.encode . toStrict . Bin.encode
+
+b64Decode :: (Typeable a, Bin.Binary a) => B.ByteString -> Either String a
+b64Decode b64 =
+    concatErr "failed to deserialize parsed base64 data: " . deserEither . BL.fromStrict =<<
+    (concatErr "failed to parse base64 data: ") (b64Decode b64)
+        where
+            b64Decode = B64.decode . padToMod4
+            concatErr e = fmapL (e ++)
+
+txtB64Encode :: Bin.Binary a => a -> T.Text
+txtB64Encode = decodeLatin1 . b64Encode
+
+txtB64Decode :: (Typeable a, Bin.Binary a) => T.Text -> Parser a
+txtB64Decode = either fail return . b64Decode . encodeUtf8
+
+parseJSONInt :: Scientific -> Parser Integer
+parseJSONInt s =
+    case toBoundedInteger s of
+        Just (CMoneyAmount i) -> return i
+        Nothing -> fail $ "failed to decode JSON number to integer. data: " ++ show s
+
+parseJSONWord :: Scientific -> Parser Word64
+parseJSONWord s =
+    case toBoundedInteger s of
+        Just w -> return w
+        Nothing -> fail $ "failed to decode JSON number to Word64. data: " ++ show s
+
+padToMod4 :: B.ByteString -> B.ByteString
+padToMod4 bs =
+    let
+        lastGroupSize = B.length bs `mod` 4
+        numPadChars = if lastGroupSize > 0 then 4 - lastGroupSize else 0
+    in
+        B.concat [bs, B.replicate numPadChars 61] -- 61: '=' ASCII
