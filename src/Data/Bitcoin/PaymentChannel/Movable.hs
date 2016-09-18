08a7f39d2055677c11a5241642099a091f3d1de3
Rune K. Svendsen <runesvend@gmail.com>
2016-09-18 14:52:36 +0200

    refine 2/2 payment receive: wrap partial-payment-received state in data type (also makes it serializable)

--- src/Data/Bitcoin/PaymentChannel/Movable.hs
+++ src/Data/Bitcoin/PaymentChannel/Movable.hs
@@ -16,7 +16,7 @@ import           Data.Bitcoin.PaymentChannel.Types
 import           Data.Bitcoin.PaymentChannel.Internal.Types
 import           Data.Bitcoin.PaymentChannel.Internal.State
 import qualified Network.Haskoin.Crypto as HC
-
+import qualified Data.Serialize         as Bin
 
 -- |A ReceiverPaymentChannel whose received value can be redeemed while
 --  keeping the channel open, by switching between two different OutPoints
@@ -26,12 +26,22 @@ data MovableChan =
         beginVal    :: BitcoinAmount
       , beginState  :: ReceiverPaymentChannel
   } | Unsettled {
-        beginVal'   ::  BitcoinAmount
-      , oldState    ::  ReceiverPaymentChannel
-      , newState    ::  ReceiverPaymentChannel
-      -- If payment 1/2 has been accepted, accept 2/2 with this function
-      , finishPay   ::  Maybe (FullPayment -> Either PayChanError (BitcoinAmount, MovableChan))
-  }
+        channels    ::  ChannelPair
+      -- If payment 1/2 has been accepted, accept 2/2 with 'receiveSecondPayment pp'
+      , finishPay   ::  Maybe PartialPayment
+  } -- deriving (Show)
+
+data ChannelPair = ChannelPair {
+    beginVal'   ::  BitcoinAmount
+  , oldState    ::  ReceiverPaymentChannel
+  , newState    ::  ReceiverPaymentChannel
+}
+
+-- |We wrap the state in which only payment 1/2 has been received in a data
+--  type, in order to be able to serialize it to disk.
+data PartialPayment =
+    NewNeedsUpdate ChannelPair BitcoinAmount
+  | OldNeedsUpdate ChannelPair BitcoinAmount
 
 newMovableChan ::
     ChannelParameters
@@ -59,7 +69,7 @@ moveChannel Unsettled{} _ _ _ = Nothing
 moveChannel (Settled v rpc) signFunc destAddr txFee =
     -- If we haven't received any value, moving the channel makes no sense
     if valueToMe rpc > 0 then
-        Just (settleTx, Unsettled v rpc newRpc Nothing)
+        Just (settleTx, Unsettled (ChannelPair v rpc newRpc) Nothing)
     else
         Nothing
     where settleTx = getSettlementBitcoinTx rpc signFunc destAddr txFee
@@ -72,8 +82,8 @@ markAsSettled ::
     MovableChan
     -> Maybe MovableChan
 markAsSettled Settled{} = Nothing
-markAsSettled (Unsettled _ _ _ (Just _)) = Nothing
-markAsSettled (Unsettled v _ newRpc Nothing) =
+markAsSettled (Unsettled _ (Just _)) = Nothing
+markAsSettled (Unsettled (ChannelPair v _ newRpc) Nothing) =
     if valueToMe newRpc > 0 then
         Just $ Settled v newRpc
     else
@@ -86,32 +96,47 @@ recvSinglePayment ::
 recvSinglePayment (Settled v rpc) fp = recvPayment rpc fp >>=
     \(a, newRpc) -> Right (a, Settled v newRpc)
 -- Accept payment 1/2
-recvSinglePayment (Unsettled v old new Nothing) fp@(CFullPayment _ op _ _)
-    | pcsPrevOut (getChannelState old) == op = updateOld (old,new,v) fp
-    | pcsPrevOut (getChannelState new) == op = updateNew (old,new,v) fp
+recvSinglePayment (Unsettled cp@(ChannelPair _ old new) Nothing) fp@(CFullPayment _ op _ _)
+    | pcsPrevOut (getChannelState old) == op = oldRecvPay cp fp
+    | pcsPrevOut (getChannelState new) == op = newRecvPay cp fp
     | otherwise = Left . OutPointMismatch . pcsPrevOut . getChannelState $ new
--- Accept payment 2/2
-recvSinglePayment (Unsettled _ _ _ (Just recvPaymentFunc)) fp = recvPaymentFunc fp
+-- If we've already received paymed 1/2, we receive 2/2 with this
+recvSinglePayment (Unsettled _ (Just pp)) fp = receiveSecondPayment pp fp
 
-updateOld ::
-    (ReceiverPaymentChannel,ReceiverPaymentChannel,BitcoinAmount)
+oldRecvPay ::
+    ChannelPair
     -> FullPayment
     -> Either PayChanError (BitcoinAmount, MovableChan)
-updateOld (old,new,v) fp = recvPayment old fp >>=
+oldRecvPay (ChannelPair v old new) fp = recvPayment old fp >>=
     -- Update old state, and leave behind a function that updates the new state
-    \(amt, newOld) -> Right (amt, Unsettled v newOld new
-        (Just $ \fp -> checkChangeValueMatch amt fp >>= updateNew (newOld,new,v)))
+    \(amt, newOld) -> Right (amt, Unsettled (ChannelPair v newOld new)
+        (Just $ NewNeedsUpdate (ChannelPair v newOld new) amt))
 
-updateNew ::
-    (ReceiverPaymentChannel,ReceiverPaymentChannel,BitcoinAmount)
+newRecvPay ::
+    ChannelPair
     -> FullPayment
     -> Either PayChanError (BitcoinAmount, MovableChan)
-updateNew (old,new,v) fp = recvPayment new fp >>=
+newRecvPay (ChannelPair v old new) fp = recvPayment new fp >>=
     -- Update new state, and leave behind a function that updates the old state
-     \(amt, newNew) -> Right (amt, Unsettled v old newNew
-        (Just $ \fp -> checkChangeValueMatch amt fp >>= updateOld (old,newNew,v)))
+     \(amt, newNew) -> Right (amt, Unsettled (ChannelPair v old newNew)
+        (Just $ OldNeedsUpdate (ChannelPair v old newNew) amt))
+
+receiveSecondPayment ::
+    PartialPayment
+    -> FullPayment
+    -> Either PayChanError (BitcoinAmount, MovableChan)
+receiveSecondPayment (OldNeedsUpdate cp amt) fp =
+    checkChangeValueMatch amt fp >>= oldRecvPay cp >>=
+        \(amt, mc) -> Right (amt, mc { finishPay = Nothing })
+receiveSecondPayment (NewNeedsUpdate cp amt) fp =
+    checkChangeValueMatch amt fp >>= newRecvPay cp >>=
+        \(amt, mc) -> Right (amt, mc { finishPay = Nothing })
 
 -- | We want to make sure that payment 1/2 and 2/2 are of equal value
 checkChangeValueMatch :: BitcoinAmount -> FullPayment -> Either PayChanError FullPayment
-checkChangeValueMatch prevValue fp@(CFullPayment (CPayment val _) _ _ _) =
-    if val /= prevValue then Left $ PartialPaymentBadValue prevValue else Right fp
+checkChangeValueMatch firstPayVal fp@(CFullPayment (CPayment val _) _ _ _) =
+    if val /= firstPayVal then Left $ PartialPaymentBadValue firstPayVal else Right fp
+
+instance Bin.Serialize MovableChan where
+        put = undefined
+        get = undefined
